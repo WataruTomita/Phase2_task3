ac6657a0ba76fc0ed91d49d4783ad6f8e9e831a5
WataruTomita <tomi.symphonia@gmail.com>
2021-02-02 14:57:02 +0900

    [finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,15 @@
 class BookCommentsController < ApplicationController
 
+  before_action :ensure_correct_comment, only: [:destroy]
+
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    # 1行に省略
+    # comment = current_user.book_comments.new(book_comment_params)
+    # 2行に分けて書くパターン↓
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
+    # ↑ここまで
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
@@ -17,5 +24,12 @@ class BookCommentsController < ApplicationController
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
+  end
+
+  def ensure_correct_comment
+    @book_comment = BookComment.find(params[:id])
+    unless @book_comment.user == current_user
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
