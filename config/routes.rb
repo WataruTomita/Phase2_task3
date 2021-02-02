335605da5f02999f0317d4da2671b414c382fccc
WataruTomita <tomi.symphonia@gmail.com>
2021-02-02 12:05:13 +0900

    fav create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
 
-  resource  :favorites, only: [:create, :destroy]
+  resources :books, only: [:new, :create, :index, :show] do
+    resource  :favorites, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
 
